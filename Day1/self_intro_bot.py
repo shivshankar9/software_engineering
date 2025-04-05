efb3817629c269cd7b76ebddebea3c8922b8d652
shivshankar9 <shivshankarkumar281@gmail.com>
2025-04-06 03:12:17 +0530

    Level 1: Week 1 - Day 1

---
+++ Day1/self_intro_bot.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+hobby = input("What's your favorite hobby? ")
+
+print("\n--- Your Intro ---")
+print("Hi! I'm " + name + ".")
+print("I'm " + age + " years old and I love " + hobby + "!")
